abbe473248fad003f8ee16dc631e88194d0c187a
Luke Palmer <lrpalmer@gmail.com>
2011-12-18 03:13:50 -0700

    First models.

--- democracy_tools/debate/models.py
+++ democracy_tools/debate/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    poster = models.User()
+    text = models.TextField()
+
+class Problem(models.Model):
+    post = models.OneToOneField(Post, primary_key=True)
+    proposal = models.ForeignKey('democracy_tools.debate.Proposal')
+
+class Proposal(models.Model):
+    post = models.OneToOneField(Post, primary_key=True)
+    problem = models.ForeignKey(Problem)
+
+class Comment(models.Model):
+    post = models.OneToOneField(Post, primary_key=True)
+    subject = models.Post(Post)
+
+    
